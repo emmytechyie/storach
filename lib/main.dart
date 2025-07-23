bead43fd9ee1cbc1399ae6a76e488c3e6f5534cb
emmytechyie <emmytechyie@gmail.com>
2025-07-23 11:28:46 +0100

    New Changes five

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:storarch/Screens/splash_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 //import 'Screens/landing_screen.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
   await Supabase.initialize(
-    url: 'https://tjlcoderpvovxytqmrox.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqbGNvZGVycHZvdnh5dHFtcm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzI1MTMsImV4cCI6MjA2NTkwODUxM30.kk2bte4j0HtbOS4D_wtOvKH6SDLS7RVm3SW3xiRX01I',
+    url: dotenv.env['https://tjlcoderpvovxytqmrox.supabase.co']!,
+    anonKey: dotenv.env[
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqbGNvZGVycHZvdnh5dHFtcm94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzI1MTMsImV4cCI6MjA2NTkwODUxM30.kk2bte4j0HtbOS4D_wtOvKH6SDLS7RVm3SW3xiRX01I']!,
   );
 
   runApp(const Storarch());
@@ -22,8 +26,6 @@ class DefaultFirebaseOptions {
 class Storarch extends StatelessWidget {
   const Storarch({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,7 +35,7 @@ class Storarch extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const SplashScreen(), 
-  );
- }
+      home: const SplashScreen(),
+    );
+  }
 }
